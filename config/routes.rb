85cc7f5ed0957087ba4f54d3163c99f75c2d6699
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-11 13:16:32 -0600

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,7 @@ Rails.application.routes.draw do
 
   get '/admin', to: 'admin#index'
 
-  # get '/admin/merchants', to: 'admin/merchants#index'
-  # post '/admin/merchants', to: 'admin/merchants#create'
-  # get '/admin/merchants/new', to: 'admin/merchants#new'
-  # get '/admin/merchants/:merchant_id', to: 'admin/merchants#show'
   patch '/admin/merchants/:merchant_id/status', to: 'admin/merchants#update_status'
-  # get '/admin/merchants/:merchant_id/edit', to: 'admin/merchants#edit'
-  # patch "/admin/merchants/:merchant_id", to: 'admin/merchants#update'
-
-  # get '/admin/invoices', to: 'admin/invoices#index'
-  # get '/admin/invoices/:invoice_id', to: 'admin/invoices#show'
-  # patch '/admin/invoices/:invoice_id', to: 'admin/invoices#update'
 
   get "/merchants/:merchant_id/dashboard", to: "merchant/dashboard#index"
   get "/merchants/:merchant_id/items", to: "merchant/items#index"
@@ -33,4 +23,6 @@ Rails.application.routes.draw do
   get '/merchants/:merchant_id/invoices/:invoice_id', to: 'merchant/invoices#show'
   patch '/merchants/:merchant_id/invoices/:invoice_id', to: 'merchant/invoices#update_status'
   patch '/merchants/:merchant_id/items/:item_id', to: 'merchant/items#update'
+
+  get 'merchants/:merchant_id/discounts', to: 'merchant/discounts#index'
 end
