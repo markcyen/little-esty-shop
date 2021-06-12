de9a1be467290b3b19303b308a359e9e57ede5b7
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-11 23:23:10 -0600

    Implement discount show page

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -4,6 +4,11 @@ class Merchant::DiscountsController < ApplicationController
     @next_three_holidays = NagerAPI.upcoming_holidays
   end
 
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.find(params[:discount_id])
+  end
+
   def new
     @merchant = Merchant.find(params[:merchant_id])
   end
