5b617878632dc6510c050d5b5ecfa5a91e21ef58
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-15 19:10:19 -0600

    Implement discounted invoice revenue

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -6,22 +6,18 @@ class Admin::InvoicesController < ApplicationController
   end
 
   def show
-    # @invoice = Invoice.find(params[:invoice_id])
-    # @customer = Customer.find(@invoice.customer_id)
-    # @invoice_items = InvoiceItem.find_invoice_items(@invoice.id)
-    # @invoice_revenue = Invoice.expected_invoice_revenue(@invoice)[0].invoice_revenue.to_f / 100
+    @invoice = Invoice.find(params[:id])
+    @all_invoice_items = @invoice.invoice_items
+    @admin_discounted_revenue = @all_invoice_items.sum do |invoice_item|
+      invoice_item.discounted_price_calculation
+    end
   end
 
   def update
-    # @invoice = Invoice.find(params[:invoice_id])
-    # @customer = Customer.find(@invoice.customer_id)
-    # @invoice_items = InvoiceItem.find_invoice_items(@invoice.id)
-    # @invoice_revenue = Invoice.expected_invoice_revenue(@invoice)[0].invoice_revenue.to_f / 100
     @invoice.update(status: params[:status])
-    # binding.pry
     @invoice.save!
+
     redirect_to "/admin/invoices/#{@invoice.id}"
-    # render :show
   end
 
   private
