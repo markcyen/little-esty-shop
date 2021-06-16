604b31cc7a222699f7fe001fe6ab32d5d0bd535b
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-15 19:09:40 -0600

    Create test for discounted revenue on admin invoices show page

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -79,4 +79,62 @@ RSpec.describe 'Admin Invoice Show' do
       expect(@invoice_1.status).to eq('completed')
     end
   end
+
+  describe 'Discounted Revenue' do
+    before :each do
+      @merchant_1 = Merchant.create!(name: "Regina's Ragin' Ragdolls")
+      @merchant_2 = Merchant.create!(name: "Mark's Money Makin' Markers")
+
+      @discount_1 = @merchant_1.discounts.create!(pct_discount: 0.10, threshold: 25)
+      @discount_2 = @merchant_1.discounts.create!(pct_discount: 0.20, threshold: 50)
+
+      @item_1 = @merchant_1.items.create!(name: "Twinkies", description: "Yummy", unit_price: 400)
+      @item_2 = @merchant_1.items.create!(name: "Applesauce", description: "Yummy in my tummy", unit_price: 340)
+      @item_3 = @merchant_2.items.create!(name: "Milk", description: "Delicious", unit_price: 400)
+      @item_4 = @merchant_2.items.create!(name: "Bread", description: "So soft", unit_price: 340)
+
+      @customer_1 = Customer.create!(first_name: "Rita", last_name: "Last Name")
+
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 0)
+      @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 1)
+      @invoice_3 = Invoice.create!(customer_id: @customer_1.id, status: 1)
+      @invoice_4 = Invoice.create!(customer_id: @customer_1.id, status: 1)
+      @invoice_5 = Invoice.create!(customer_id: @customer_1.id, status: 0)
+
+      # Scenario 1
+      @invoice_item_1 = InvoiceItem.create!(item: @item_1, invoice: @invoice_1, quantity: 24, unit_price: 400, status: 1)
+      @invoice_item_2 = InvoiceItem.create!(item: @item_2, invoice: @invoice_1, quantity: 19, unit_price: 340, status: 0)
+
+      # Scenario 2
+      @invoice_item_3 = InvoiceItem.create!(item: @item_1, invoice: @invoice_2, quantity: 26, unit_price: 400, status: 1)
+      @invoice_item_4 = InvoiceItem.create!(item: @item_2, invoice: @invoice_2, quantity: 19, unit_price: 340, status: 1)
+
+      # Scenario 3
+      @invoice_item_5 = InvoiceItem.create!(item: @item_1, invoice: @invoice_3, quantity: 25, unit_price: 400, status: 1)
+      @invoice_item_6 = InvoiceItem.create!(item: @item_2, invoice: @invoice_3, quantity: 50, unit_price: 340, status: 1)
+
+      # Scenario 4
+      @invoice_item_7 = InvoiceItem.create!(item: @item_1, invoice: @invoice_4, quantity: 51, unit_price: 400, status: 1)
+      @invoice_item_8 = InvoiceItem.create!(item: @item_2, invoice: @invoice_4, quantity: 50, unit_price: 340, status: 1)
+
+      # Scenario 5
+      @invoice_item_9 = InvoiceItem.create!(item: @item_1, invoice: @invoice_5, quantity: 25, unit_price: 400, status: 1)
+      @invoice_item_10 = InvoiceItem.create!(item: @item_2, invoice: @invoice_5, quantity: 50, unit_price: 340, status: 1)
+      @invoice_item_11 = InvoiceItem.create!(item: @item_3, invoice: @invoice_5, quantity: 25, unit_price: 400, status: 1)
+      @invoice_item_12 = InvoiceItem.create!(item: @item_4, invoice: @invoice_5, quantity: 50, unit_price: 340, status: 1)
+
+      Transaction.create!(invoice_id: @invoice_1.id, result: 0, credit_card_number: '12345', credit_card_expiration_date: '12345')
+      Transaction.create!(invoice_id: @invoice_2.id, result: 0, credit_card_number: '12345', credit_card_expiration_date: '12345')
+      Transaction.create!(invoice_id: @invoice_3.id, result: 0, credit_card_number: '12345', credit_card_expiration_date: '12345')
+      Transaction.create!(invoice_id: @invoice_4.id, result: 0, credit_card_number: '12345', credit_card_expiration_date: '12345')
+      Transaction.create!(invoice_id: @invoice_5.id, result: 0, credit_card_number: '12345', credit_card_expiration_date: '12345')
+    end
+
+    it 'displays discounted revenue' do
+      visit "/admin/invoices/#{@invoice_5.id}"
+
+      expect(page).to have_content("Expected Invoice Revenue: $540.00")
+      expect(page).to have_content("Discounted Invoice Revenue: $496.00")
+    end
+  end
 end
